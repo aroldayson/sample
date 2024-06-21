ea454812ce583730e5c2f22ff3bed5d4f21b869f
Arold Ayson <aroldayson3@gmail.com>
2024-06-21 08:12:25 +0800

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/react-logo.png')}
           style={styles.reactLogo}
         />
       }>
@@ -44,6 +44,7 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          <ThemedText type="defaultSemiBold">sample-app</ThemedText>.
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -61,10 +62,10 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    margin: 'auto'
   },
 });
